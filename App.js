0dabe7bbdde61baebdc4ca4b4ba18fa930158d2e
iceycc <w@icey.cc>
2017-11-28 21:44:46 +0800

    1-项目构建

---
+++ App.js
@@ -0,0 +1,18 @@
+
+
+
+var App = {
+  template:`
+  <div>
+    //英雄列表
+    <hero-list></hero-list>
+    //添加英雄
+
+  
+  </div>
+  
+  `
+
+
+
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+new Vue({
+  el:"#app",
+  render:c=c(App)
+
+})
